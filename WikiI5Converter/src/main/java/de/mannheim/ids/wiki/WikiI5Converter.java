48685ccd30301241f3a3eb310d8865a9ef434aa1
margaretha <margaretha@ids-mannheim.de>
2014-06-25 12:26:58 +0200

    update inflectives

--- WikiI5Converter/src/main/java/de/mannheim/ids/wiki/WikiI5Converter.java
+++ WikiI5Converter/src/main/java/de/mannheim/ids/wiki/WikiI5Converter.java
@@ -3,15 +3,15 @@ package de.mannheim.ids.wiki;
 import java.io.File;
 
 /** This is the main class for converting the XML-ized Wikipages to XCES
- *  This class takes 7 arguments as inputs:
+ *  This class takes 6 arguments as inputs:
  *  1. the folder path of the XML-ized Wikipages,
  *  2. the type of the pages (articles or discussions), 
  *  3. the filename of the Wikipedia dump in format:
  *     [2 character language code]wiki-[year][month][date]-pages-meta-current.xml
  *     example: dewiki-20130728-pages-meta-current.xml
- *  4. the output file,   
- *  5. the path to the XML file containing the list of inflectives.
- *  6. the encoding of the output file, for example UTF-8 or ISO-8859-1
+ *  4. the output file,
+ *  5. the encoding of the output file, for example UTF-8 or ISO-8859-1
+ *  6. the path to the XML file containing the list of inflectives.
  *  
  * @author margaretha 
  */
@@ -23,9 +23,9 @@ public class WikiI5Converter {
 		String xmlFolder=args[0];
 		String type=args[1];
 		String dumpFilename=args[2];
-		String outputFile=args[3];
-		String inflectives=args[4];
-		String encoding=args[5];
+		String outputFile=args[3];		
+		String encoding=args[4];
+		String inflectives=args[5];
 		
 		/*String dumpFilename="dewiki-20130728-pages-meta-current.xml";
 		String outputFile="output.xces";
@@ -44,6 +44,10 @@ public class WikiI5Converter {
 		System.setProperty("entityExpansionLimit", "0");
 		System.setProperty("totalEntitySizeLimit", "0");
 		System.setProperty("PARAMETER_ENTITY_SIZE_LIMIT", "0");
+		
+		if (inflectives.equals("null")){
+			inflectives = null;
+		}		
 		
 		WikiI5Processor wikiXCESProcessor = new WikiI5Processor(xmlFolder,xsl,
 				type,dumpFilename,inflectives,encoding);
